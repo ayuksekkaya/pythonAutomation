201552e936f513c3ad3e3e4706d835839e944432
Ali Yuksekkaya <64444373+ayuksekkaya@users.noreply.github.com>
2022-09-28 15:26:12 -0400

    Create before.sh
    add early commands

---
+++ before.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+mkdir -p /run/oldroot
+mkdir -p /run/ssh
+
+mount /dev/mapper/ubuntu--vg-ubuntu--lv /run/oldroot/
+
+if [ -d /run/oldroot/etc/ssh ]
+  then
+    # Save old SSH keys to RAM filesystem. We will restore them in the %post section
+    cp -v -a /run/oldroot/etc/ssh/{moduli,ssh_host_*} /run/ssh/
+    echo "Copied old ssh keys to RAM"
+
+  else
+    echo "No old ssh directory found"
+fi
