4d1c807574f2330262d22660deacddd8438f3ea5
Ali Yuksekkaya <64444373+ayuksekkaya@users.noreply.github.com>
2022-09-29 22:47:36 -0400

    Update before.sh

--- before.sh
+++ before.sh
@@ -8,9 +8,13 @@ mount /dev/mapper/ubuntu--vg-ubuntu--lv /run/oldroot/
 if [ -d /run/oldroot/etc/ssh ]
   then
     # Save old SSH keys to RAM filesystem. We will restore them in the %post section
-    cp -v -a /run/oldroot/etc/ssh/{moduli,ssh_host_*} /run/ssh/
+    cp -v -a /run/oldroot/etc/ssh/moduli /run/ssh/
+    
+    for i in /run/oldroot/etc/ssh/ssh_host_*
+    do
+       cp -v -a $i /run/ssh/
+    done
     echo "Copied old ssh keys to RAM"
-
   else
     echo "No old ssh directory found"
 fi
