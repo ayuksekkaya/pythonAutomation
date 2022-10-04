fc9bedfa64eec426daf760254161fa9f68310ece
Ali Yuksekkaya <64444373+ayuksekkaya@users.noreply.github.com>
2022-10-04 15:15:42 -0400

    Update restore_ssh.sh

--- restore_ssh.sh
+++ restore_ssh.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 # restore SSH keys
+mkdir /target/root/ssh_host_keys/
+
 KEYS=$(ls /run/ssh/ssh_host_* | wc -l)
 if [ $KEYS -ge 6 ]
   then
-    cp -v -a '/run/ssh/'* /target/etc/ssh
+    cp -v -a '/run/ssh/'* /target/root/ssh_host_keys/
 fi
