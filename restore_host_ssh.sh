aef6cb223eeb64e3e65a4ca0ff8848b3908841e4
Ali Yuksekkaya <64444373+ayuksekkaya@users.noreply.github.com>
2022-10-04 15:33:37 -0400

    Create restore_host_ssh.sh

---
+++ restore_host_ssh.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+
+cp -v -a '/root/ssh_host_keys/'* /etc/ssh/
+
+systemctl disable restore_host_ssh.service 
+rm -rf /etc/systemd/system/restore_host_ssh.service
+rm -f /root/restore_host_ssh.sh.sh
