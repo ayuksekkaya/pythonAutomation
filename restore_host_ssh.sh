cecac1d469a500be1a2404c84153dab32a7f77b4
Ali Yuksekkaya <64444373+ayuksekkaya@users.noreply.github.com>
2022-10-13 15:54:38 -0400

    Update restore_host_ssh.sh

--- restore_host_ssh.sh
+++ restore_host_ssh.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
 
-cp -v -a '/root/ssh_host_keys/'* /etc/ssh/
+KEYS=$(ls /run/ssh/ssh_host_* | wc -l)
+if [ $KEYS -ge 6 ]
+  then
+    cp -v -a '/root/ssh_host_keys/'* /etc/ssh/
+    systemctl disable restore_host_ssh.service 
+    rm -rf /etc/systemd/system/restore_host_ssh.service
+    rm -f $0
+    touch /root/host_keys_restored
+    else
+        echo "No old ssh directory found"
+fi
 
-systemctl disable restore_host_ssh.service 
-rm -rf /etc/systemd/system/restore_host_ssh.service
-rm -f /root/restore_host_ssh.sh
-touch /root/host_keys_restored
+lvextend -l +100%FREE /dev/mapper/ubuntu--vg-ubuntu--lv
+resize2fs /dev/mapper/ubuntu--vg-ubuntu--lv
